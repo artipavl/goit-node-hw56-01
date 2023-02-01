edfc21cda409c927072d2561a07f3c84b0a589b8
artipavl <40296816+artipavl@users.noreply.github.com>
2023-02-01 18:23:50 +0200

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log("rrr555r");
+
+// index.js
+const argv = require("yargs").argv;
+
+// TODO: рефакторить
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      // ...
+      break;
+
+    case "get":
+      // ... id
+          console.log(id)
+      break;
+
+    case "add":
+      // ... name email phone
+      break;
+
+    case "remove":
+      // ... id
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
